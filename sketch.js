9ce1a0382e4304d93e773b64acd67948b2e37c0c
Jack WIlde <jackwilde630@gmail.com>
2015-11-26 14:53:46 -0500

    semi-functional clear button

--- sketch.js
+++ sketch.js
@@ -1,22 +1,46 @@
 $(document).ready(function(){
 
-	$('body').prepend('<div id="container"></div>')
-	$('#container').prepend('<button>Click me!</button>')
+	$('body').prepend('<div id="container"></div>');
+	$('body').prepend('<button id="clear">Clear</button>');
 
 	var genDivs = function(size){
-		for (var i = 1; i < size; i++){
+		for (var i = 1; i <= size; i++){
 			$('#container').append('<div class="row"></div>');
-				for (var j =1; j < size; j++){
-					$('.row').last().append('<div class="cell"></div>')
+				for (var j =1; j <= size; j++){
+					$('.row').last().append('<div class="cell"></div>');
 				}
-			
-		} 
+		$('.cell').css({'width': '50px', 'height': '50px'});
+		$('.row').css({'width': '50px', 'display': 'block', 'float': 'left'});	
+		}
+		$('.cell').hover(function(){
+			$(this).css({'background-color': '#303030'});
+	});
+ 
 	}
-	
 
+	var buttonPress = function(){
+		$('.row').remove();
+		var pixels = 0 + prompt('how big we talkin?');
+		genDivs(pixels);
+	}
 
 	genDivs(16)
 
-	$('.cell').css({'width': '50px', 'height': '50px', 'background-color': 'blue'})
-	$('.row').css({'width': '50px', 'display': 'block', 'float': 'left'})
+	$('#clear').on('click', function(){
+		buttonPress()
+	})
+
+	
+	
+	$('#container').css({'display': 'block', 
+						'border': '3px solid black', 
+						'margin-left': 'auto',
+						'margin-right': 'auto',
+						'width': '800px',
+						'height': '800px'});
+	$('button').css({'display': 'block',
+						'margin-left': 'auto',
+						'margin-right': 'auto',
+						'margin-bottom': '50px'})
+
 }); 
